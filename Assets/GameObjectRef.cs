5798e264fae683882df2dd217e674ed591e94585
Jeremy Blouir <jrmyblouir@gmail.com>
2017-07-23 15:07:02 -0400

    Intial Commit

---
+++ Assets/GameObjectRef.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectRef : MonoBehaviour
+{
+	public GameObject game_object;
+}
---
+++ Assets/game/core.clj
@@ -0,0 +1,174 @@
+(ns game.core
+  (:use arcadia.core
+        arcadia.linear))
+
+(defn new-extrema [min max]
+  {:minimum min :maximum max})
+
+(def columns 8)
+(def rows 8)
+
+(def wall-count (new-extrema 5 9))
+(def food-count (new-extrema 1 5))
+
+(defn get-object
+  "Gets the GameObject referenced by GameObjectRef"
+  [go-ref]
+  (.(. go-ref (GetComponent "GameObjectRef"))
+    game_object))
+
+(defn get-list
+  "Gets the GameObject[] list from the GameObjectList component"
+  [go-list-ref]
+  (. (. go-list-ref (GetComponent "GameObjectList"))
+     game_objects))
+
+(defn set-parent-go!
+  "Convenience method for parenting at the GameObject level"
+  [child-go parent-go]
+  (. (. child-go transform)
+     (SetParent (. parent-go transform))))
+
+(defn set-parent-go-list!
+  "Same as set-parent-go! just works on vectors"
+  [child-list parent]
+  (doall
+   (for [child child-list]
+     (set-parent-go! child parent))))
+
+(defn state-obj
+  "Get the GameObject referenced from the GameObjectRef with state key" 
+  ([go kw]
+   (get-object (state go kw))))
+
+(defn state-list
+  "Get the GameObject[] list from the GameObjectList with state key"
+  ([go kw]
+   (get-list (state go kw))))
+
+(defn qidentity [] (.. UnityEngine.Quaternion identity))
+
+(defn rand-int-range [min max]
+  (+ (rand-int (- max min)) min))
+
+(defn rand-int-extrema [extrema]
+  (rand-int-range (:minimum extrema) (:maximum extrema)))
+
+(defn random-selection
+  "Takes a range of potential objects and a minima 
+   maxima extrema and returns a random selection of
+   non-unique objects within the extremes"
+  [objects quantity]
+  (let [object-count quantity] ;; Random count
+    (loop [total 0 result []]
+      (if (or (>= total object-count) (= object-count 0))
+        result             
+        (recur (inc total) (conj result (nth objects (rand-int (count objects)))))))))
+
+(defn log-b
+  "Wrapper for the Unity Mathf.Log function, calls log n to base b"
+  [n b]
+  (int (.. UnityEngine.Mathf (Log n b))))
+
+
+(defn line-points
+  "Basic function to generate a line of points"
+  [start-x start-y inc-x inc-y length]
+  (into [] (for [i (range length)]
+             (v3 (+ (* inc-x i) start-x)
+                 (+ (* inc-y i) start-y) 0))))
+
+(defn square-outline-points
+  "Generates a set of points on the outside edges of a square"
+  [min-x max-x min-y max-y]
+  (let [width (inc (- max-x min-x))
+        height (inc (- max-y min-y))]
+    (into [] (concat (line-points min-x max-y 1 0 width) ;; top line
+                     (line-points max-x (- max-y 1) 0 -1 (- height 2)) ;; right line
+                     (line-points max-x min-y -1 0 width) ;; bottom line
+                     (line-points min-x (+ min-y 1) 0 1 (- height 2)))))) ;; left line
+
+(defn square-points
+  "Generates a square/grid of positions given the x and y extrema"
+  [min-x max-x min-y max-y]
+  (into [] (for [col (range min-x max-x)
+                 row (range min-y max-y)]
+             (v3 col row 0))))
+
+(defn layout-objects!
+  "Instantiates each GameObject at the given point, returns instantiated gameobjects"
+  [object-list points]
+  (loop [objects object-list
+         positions points
+         game-objects []]
+    (if (or (= (count objects) 0) (= (count positions) 0))
+      game-objects             
+      (recur (pop objects)
+             (pop positions)
+             (conj game-objects (instantiate (last objects) (last positions) (qidentity)))))))
+
+(defn board-setup! [board-holder floor-tiles outer-wall-tiles]
+  (let [outer-wall-positions (square-outline-points -1 columns -1 rows)
+        outer-wall-objects (random-selection outer-wall-tiles (+ columns columns rows rows 4))
+        floor-positions (square-points 0 columns 0 rows)
+        floor-objects (random-selection floor-tiles (* columns rows))]
+    (do
+      (set-parent-go-list! (layout-objects! outer-wall-objects outer-wall-positions)
+                           board-holder)
+      (set-parent-go-list! (layout-objects! floor-objects floor-positions)
+                           board-holder))))
+
+(defn layout-objects-lists!
+  "Takes multiple object lists and calls layout-objects on each carrying
+   over the remaining points to each call"
+  [object-lists points]
+  (if (= (count object-lists) 0)
+    nil
+    (let [object-list (last object-lists)
+          object-length (count object-list)]
+      (do
+        (layout-objects! object-list (subvec points 0 object-length))
+        (recur (pop object-lists) (subvec points object-length))))))
+
+(defn setup-scene! [level board-manager]
+  (let [floor-tiles (state-list board-manager :floor-tiles)
+        outer-wall-tiles (state-list board-manager :outer-wall-tiles)
+        wall-tiles (state-list board-manager :wall-tiles)
+        food-tiles (state-list board-manager :food-tiles)
+        enemy-tiles (state-list board-manager :enemy-tiles)
+        exit (state-obj board-manager :exit-ref)
+        board (state-obj board-manager :board-holder-ref)
+        enemy-count (log-b level 2)
+        positions (shuffle (square-points 1 (- columns 2)
+                                          1 (- rows 2)))]
+    (do (board-setup! board floor-tiles outer-wall-tiles)
+        (layout-objects-lists!
+         [(random-selection food-tiles (rand-int-extrema food-count))
+          (random-selection wall-tiles (rand-int-extrema wall-count))
+          (random-selection enemy-tiles enemy-count)]
+         positions)
+        (instantiate exit (v3 (dec columns) (dec rows) 0) (qidentity))
+        )))
+
+(defn board-start [go]
+  (setup-scene! 1 go))
+
+;; Examples
+(comment (defn board-start [gameobject]
+   (doall (for [x (range 8)]
+            ((instantiate                                   
+               (.. UnityEngine.Resources (Load (nth wallNames x)))
+                 (v3 x x 0)
+                 (.. UnityEngine.Quaternion identity)))))))
+
+(defn collision-example [go collision]
+  (arcadia.core/log (.. go name)
+       " collided with "
+       (.. collision gameObject name)
+       " at velocity "
+       (.. collision relativeVelocity)))
+
+(defn rotate-example [gameobject] 
+  (.. gameobject transform (Rotate 0 4 0)))
+
+
